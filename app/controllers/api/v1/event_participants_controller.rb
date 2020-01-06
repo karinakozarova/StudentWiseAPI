42ba9801128d85b162b94960bcd7738dd15714b0
Jonasz Kądziela <j.kadziela@student.fontys.nl>
2020-01-06 17:16:58 +0100

    Added EventParticipants controller

---
+++ app/controllers/api/v1/event_participants_controller.rb
@@ -0,0 +1,32 @@
+class Api::V1::EventParticipantsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_event
+  before_action :set_event_participant, only: :destroy
+
+  def create
+    @event_participant = EventParticipant.new(event_participant_params)
+    @event_participant.event_id = @event.id
+
+    @event_participant.save!
+    head :created
+  end
+
+  def destroy
+    @event_participant.destroy
+    head :ok if @event_participant.destroyed?
+  end
+
+  private
+
+  def set_event
+    @event = Event.created_by(current_user).find(params[:event_id])
+  end
+
+  def set_event_participant
+    @event_participant = EventParticipant.find_by!(event_id: @event.id, participant_id: event_participant_params[:participant_id])
+  end
+
+  def event_participant_params
+    params.require(:event_participant).permit(:participant_id)
+  end
+end
