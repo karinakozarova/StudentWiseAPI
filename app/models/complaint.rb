992838019b68f8e7098f2509678a509a3d2b1f29
Jonasz Kądziela <j.kadziela@student.fontys.nl>
2020-01-12 01:24:18 +0100

    Added Complaints controller

--- app/models/complaint.rb
+++ app/models/complaint.rb
@@ -1,6 +1,8 @@
 class Complaint < ApplicationRecord
   STATUSES = %i(in_progress received rejected resolved sent).freeze
 
+  scope :with_creator, ->(user) { where(creator_id: user.id) }
+
   belongs_to :creator, class_name: 'User'
 
   validates :status, inclusion: { in: Complaint::STATUSES.map(&:to_s) }
