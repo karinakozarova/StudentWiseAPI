36dc9f3d33c6d679eb6676eace9107e0a110fb20
Jonasz Kądziela <j.kadziela@student.fontys.nl>
2020-01-14 21:16:21 +0100

    Added integration specs for new Expense actions

--- spec/factories/v1/expenses.rb
+++ spec/factories/v1/expenses.rb
@@ -5,5 +5,9 @@ FactoryBot.define do
     notes { Faker::Lorem.sentence }
     price { Faker::Number.decimal(l_digits: 2) }
     amount { Faker::Number.between(from: 1, to: 10) }
+    archived { Faker::Boolean.boolean }
+
+    trait(:archived) { archived { true } }
+    trait(:unarchived) { archived { false } }
   end
 end
