9a361ee19841470e2a259725a03ac697eb5c31b1
Jonasz Kądziela <j.kadziela@student.fontys.nl>
2020-01-16 20:16:52 +0100

    Added integration specs for new Complaint actions

---
+++ spec/integration/v1/complaints/mark_as_rejected_spec.rb
@@ -0,0 +1,38 @@
+require 'swagger_helper'
+
+RSpec.describe 'Mark an Complaint as rejected', swagger_doc: 'v1/swagger.json' do
+  let(:user) { create(:user, :admin) }
+  let(:complaint) { create(:complaint) }
+  let(:auth_token) { user_auth_token(user) }
+
+  path '/api/v1/complaints/{id}/mark_as_rejected' do
+    put 'Marks an complaint as rejected' do
+      tags 'Complaints'
+      security [ Bearer: [] ]
+      parameter name: :id,
+        in: :path,
+        type: :integer
+
+      response '200', 'complaint marked as rejected' do
+        let(:Authorization) { auth_token }
+        let(:id) { complaint.id }
+
+        run_test!
+      end
+
+      response '401', 'unauthorized' do
+        let(:Authorization) { 'invalid' }
+        let(:id) { complaint.id }
+
+        run_test!
+      end
+
+      response '404', 'not found' do
+        let(:Authorization) { auth_token }
+        let(:id) { 0 }
+
+        run_test!
+      end
+    end
+  end
+end
