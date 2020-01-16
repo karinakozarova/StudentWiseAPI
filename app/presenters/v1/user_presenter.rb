28208560fd41067b1b063d82be25200bdf308442
Jonasz Kądziela <j.kadziela@student.fontys.nl>
2020-01-16 15:40:15 +0100

    Adjusted User partial to show admin field

--- app/presenters/v1/user_presenter.rb
+++ app/presenters/v1/user_presenter.rb
@@ -2,6 +2,6 @@ class V1::UserPresenter < ApplicationPresenter
   presents :user
 
   def privileged?
-    v.current_user.id == user.id
+    v.current_user.id == user.id || v.current_user.admin?
   end
 end
