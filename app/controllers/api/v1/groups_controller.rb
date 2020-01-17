eb743587397ca9afbd746f35b0bc2f78082a63dd
Jonasz Kądziela <j.kadziela@student.fontys.nl>
2020-01-17 14:18:45 +0100

    Added option to move and remove Users from Groups

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::GroupsController < ApplicationController
   before_action :authenticate_user!, except: %i(index show)
   before_action :require_admin!, only: %i(create update destroy)
-  before_action :set_group, only: %i(show update destroy)
+  before_action :set_group, except: %i(index create)
+  before_action :set_user, only: %i(move_member remove_member)
   before_action :check_group, only: %i(update destroy)
 
   def index
@@ -27,14 +28,32 @@ class Api::V1::GroupsController < ApplicationController
     @group.destroy
   end
 
+  def move_member
+    @user.update!(group_id: @group.id)
+    render :member, status: :ok
+  end
+
+  def remove_member
+    @user.update!(group_id: nil)
+    render :member, status: :ok
+  end
+
   private
 
   def set_group
-    @group = Group.find(params[:id])
+    @group = Group.find(params[:id] || params[:group_id])
+  end
+
+  def set_user
+    @user = User.find(group_member_params[:member_id])
   end
 
   def group_params
     params.require(:group).permit(:name, :description, :rules)
+  end
+
+  def group_member_params
+    params.require(:group_member).permit(:member_id)
   end
 
   def check_group
